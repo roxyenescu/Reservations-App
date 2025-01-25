34f8bb5711f98ce49c438a9f25fd58ecc5b522a3
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-01-26 01:11:32 +0200

    Configurare variabile de mediu si sistem de logging

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
-const express = require('express');
+require('dotenv').config(); 
+
+const express = require('express'); 
 const logger = require('./src/middlewares/logger');
+const morgan = require('morgan');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Server is running');
