45769855bb15490fa8571e066895cae937ccaeb6
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-01-26 01:01:57 +0200

    Adaugare middleware-uri esentiale si logging

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const logger = require('./src/middlewares/logger');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
 
 app.get('/', (req, res) => {
     res.send('Server is running');
